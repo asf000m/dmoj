871545f176821b51b0b9dac30e7426c4f5e7fb51
asf000m <asf000m@gmail.com>
2026-04-22 22:54:58 -0300

    Change Problem ccc08j2.py - CCC '08 J2 - Do the Shuffle

--- ccc08j2.py
+++ ccc08j2.py
@@ -8,17 +8,16 @@ while True:
     button = int(input())
     times = int(input())
 
-    if button == 1:
-        for _ in range(times):
-            playlist = playlist[1:] + playlist[0]
-    elif button == 2:
-        for _ in range(times):
-            playlist = playlist[-1] + playlist[:-1]
-    elif button == 3:
-        for _ in range(times):
-            playlist = playlist[1] + playlist[0] + playlist[2:]
-    elif button == 4:
+    if button == 4:
         break
+    
+    for _ in range(times):
+        if button == 1:
+            playlist = playlist[1:] + playlist[0]
+        elif button == 2:
+            playlist = playlist[-1] + playlist[:-1]
+        elif button == 3:
+            playlist = playlist[1] + playlist[0] + playlist[2:]
 
 output = ''
 for song in playlist:
